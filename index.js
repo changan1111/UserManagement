9b31ebb527e6f8cde7dcb1b5d20b4cf161ea3f8f
changan1111 <80892371+changan1111@users.noreply.github.com>
2022-10-11 13:19:52 +0530

    cors

--- index.js
+++ index.js
@@ -18,12 +18,9 @@ app.use(express.json());
 var cors = require('cors');
 
 
-app.use(cors({
-  origin: '*'
-}));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
