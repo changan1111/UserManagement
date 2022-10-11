2e270eab4b168e977067d1a24a285a1266038119
changan1111 <80892371+changan1111@users.noreply.github.com>
2022-10-11 15:05:28 +0530

    xhr issue

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ app.use(express.json());
 
 var cors = require('cors');
 
-
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
