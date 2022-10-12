433a327bc0c4a72f59a6e8ac950f47e6de062603
changan1111 <80892371+changan1111@users.noreply.github.com>
2022-10-12 15:31:35 +0530

    index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
 
 app.use(function(req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
