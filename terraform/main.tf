ba12c4c1f6a9ae5b0097a9044d2bcbcc5b4895c7
changan1111 <80892371+changan1111@users.noreply.github.com>
2022-12-19 14:32:33 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,23 +13,42 @@ provider "aws" {
 resource "aws_instance" "servernode" {
   ami                    = "ami-09d3b3274b6c5d4aa"
   instance_type          = "t2.micro"
-  #key_name               = aws_key_pair.deployer.key_name
- # connection {
- #   type        = "ssh"
- #   host        = self.public_ip
- #   user        = "ubuntu"
- #   private_key = var.private_key
- #   timeout     = "4m"
-#}
+  key_name               = "vpcpublickey"
+
+connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = var.private_key
+    timeout     = "4m"
+  }
   tags = {
     "name" = "DeployVM"
   }
 }
 
-#resource "aws_key_pair" "deployer" {
-#  key_name   = var.key_name
-#  public_key = var.public_key
-#}
+resource "aws_security_group" "alb_security_group"{
+  name="alb security group"
+  ingress {
+    from_port=22
+    to_port=22
+    protocol="tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description="ssh access"
+
+}
+  egress {
+    from_port=0
+    to_port=0
+    protocol="-1"
+    cidr_blocks=["0.0.0.0/0"]
+  }
+
+  tags={
+    name="alb_security_group"
+  }
+}
+
 
 output "instance_public_ip" {
   value     = aws_instance.servernode.public_ip
